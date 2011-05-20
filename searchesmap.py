a1c2cbfa55d90fdd7e5e30febed2b65501e65c38
Valkyrie Savage <vasavage@gmail.com>
2011-05-20 12:39:17 -0700

    working python thing which parses searches

--- searchesmap.py
+++ searchesmap.py
@@ -7,6 +7,11 @@ import sys
 GI = GeoIP("/home/valkyrie/projects/inspire-log-viz/src/GeoLiteCity.dat")
 LOG_TO_READ = "/home/valkyrie/projects/inspire-log-viz/log.sample"
 IP_REGEX = re.compile(r'(\d{1,3}.){4}')
+SEARCH_REGEX = re.compile(r'(p|p1)=(?P<search>.*?)(&|\s)')
+
+# URL DECODE from http://jaytaylor.com/blog/2010/04/25/urldecode-for-python-one-liner/
+_UD = re.compile('%([0-9a-hA-H]{2})', re.MULTILINE)
+URLDECODE = lambda x: _UD.sub(lambda m: chr(int(m.group(1), 16)), x)
 
 def get_lat_long_pair(ip):
     record = GI.record_by_addr(ip)
@@ -14,18 +19,32 @@ def get_lat_long_pair(ip):
         return (0,0)
     return (record['latitude'], record['longitude'])
 
-def pull_ip_list_from_log():
+def get_search_from_line(line):
+    return SEARCH_REGEX.search(line).group('search')
+
+def prettify_search(search):
+    search = search.lower()
+    search = search.replace('+', ' ')
+    search = URLDECODE(search)
+    return search
+
+def pull_list_from_log():
     log = open(LOG_TO_READ)
-    ips = []
+    ips_and_searches = []
     for line in log:
         if not "GET /search?" in line:
             continue
         ip = line.split(' - ')[0]
-        if IP_REGEX.match(ip):
-            ips.append(ip)
-    return ips
+        if not IP_REGEX.match(ip):
+            continue
+        search = prettify_search(get_search_from_line(line))
+        ips_and_searches.append((ip, search))
+    return ips_and_searches
 
-ips = pull_ip_list_from_log()
-for ip in ips:
+print "Content-type: text/html"
+print
+
+ips_and_searches = pull_list_from_log()
+for ip, search in ips_and_searches:
     latitude, longitude = get_lat_long_pair(ip)
-    print str(latitude) + ' ' + str(longitude)
+    print str(latitude) + ' ' + str(longitude) + ' ' + search + '<br>'
