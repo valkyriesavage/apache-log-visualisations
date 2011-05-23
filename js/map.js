37884728e892ed42cbaff48be999165794144f0d
Valkyrie Savage <vasavage@gmail.com>
2011-05-23 14:25:17 -0700

    clustering added but not toggleable, searches displayed, speed changeable, pauseable

--- js/map.js
+++ js/map.js
@@ -1,40 +1,78 @@
 var map = null;
+var clusterer = null;
+
 var ajaxRequest = null;
+
 var pointsToMap = [];
-var mapped = 0;
-var running = false;
+var markersAdded = [];
+
+var clustering = false;
+var running = true;
+var speed = .7;
 
 function initialize_map() {
     var SLAC = new google.maps.LatLng(37.418265, -122.2008149);
     var worldCentredOnSLAC = {
-      zoom: 6,
+      zoom: 2,
       center: SLAC,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
     map = new google.maps.Map(document.getElementById("map_canvas"),
             worldCentredOnSLAC);
-    toggle_run();
-    request_lat_longs_ajax();
+    //toggle_clustering();
+    wait_a_moment();
 }
 
-function toggle_run() {
+function toggle_clustering() {
+    clustering = !clustering;
+    if (clustering) {
+        if (!clusterer) {
+            clusterer = new MarkerClusterer(map);
+        }
+        else {
+            clusterer.addMarkers(markersAdded);
+        }
+    }
+    else {
+        clusterer.clearMarkers();
+        for (marker in markersAdded) {
+            marker.setVisible(true);
+        }
+    }
+}
+
+function toggle_run(button) {
     running = !running;
+    if (!running) {
+        button.innerHTML = 'start';
+    }
+    else {
+        button.innerHTML = 'pause';
+    }
+}
+
+function change_speed(newSpeed) {
+    speed = newSpeed/10;
+}
+
+function update_searched(search) {
+    document.getElementById('searched').innerHTML = '<a href="http://inspirebeta.net/search?p=' +
+            encodeURI(search.replace(/ /g, '+')) + '">' + search + '</a>';
 }
 
 function request_lat_longs_ajax(){
-    $.getJSON('http://inspire-viz.com/python/searchesmap.py?mapped='+mapped,
-              {},
-              read_lat_longs_ajax);
-    console.log('request sent to server');
+    if (running) {
+        $.getJSON('http://inspire-viz.com/python/searchesmap.py?mapped='+markersAdded.length,
+                  {},
+                  read_lat_longs_ajax);
+    }
     wait_a_moment();
 }
 
 function wait_a_moment() {
-    if (running) {
-        setTimeout("request_lat_longs_ajax()", 1000);
-        setTimeout("map_new_point();", 1000);
-    }
+    setTimeout("request_lat_longs_ajax();", speed*3000);
+    setTimeout("map_new_point();", speed*3000);
 }
 
 function read_lat_longs_ajax(data, textStatus, jqXHR) {
@@ -42,25 +80,25 @@ function read_lat_longs_ajax(data, textStatus, jqXHR) {
         console.log('no response from server')
     }
     else {
-        console.log('point retrieved');
         pointsToMap.push(data);
     }
 }
 
 function map_new_point() {
     if (pointsToMap.length < 1) {
-        console.log('no points to map');
         return 1;
+    }
+    if (!running) {
+        return 0;
     }
     point_and_search = pointsToMap.shift();
     latitude = point_and_search['latitude'];
     longitude = point_and_search['longitude'];
     search = point_and_search['search'];
-    drop_point(map, latitude, longitude, search);
-    mapped += 1;
+    drop_point(latitude, longitude, search);
 }
 
-function drop_point(map, latitude, longitude, search) {
+function drop_point(latitude, longitude, search) {
     accessedPoint = new google.maps.LatLng(latitude, longitude);
     if (search.search('ellis') > -1) {
         marker = new google.maps.Marker({
@@ -68,7 +106,7 @@ function drop_point(map, latitude, longitude, search) {
             draggable:true,
             animation: google.maps.Animation.DROP,
             position: accessedPoint,
-            icon: 'images/john-ellis.jpg',
+            icon: 'http://inspire-viz.com/images/john-ellis.jpg',
             title: search,
         });
     }
@@ -80,6 +118,12 @@ function drop_point(map, latitude, longitude, search) {
             position: accessedPoint,
             title: search,
         });
+    }
+    marker.click = "update_searched('"+marker.title+"');";
+    update_searched(search);
+    markersAdded.push(marker);
+    if (clustering) {
+        clusterer.addMarker(marker);
     }
     //map.setCenter(accessedPoint);
 }
