9698860040f3ff9bd2c2520582871c26e0cb0841
Valkyrie Savage <vasavage@gmail.com>
2011-06-13 15:54:19 -0700

    webgl globe seems done, cannot test because graphics card does not support webgl

--- js/globe.js
+++ js/globe.js
@@ -3,17 +3,21 @@ var globe = null;
 function initialize_globe() {
     var globe_container = document.getElementById('globe');
     globe = new DAT.Globe(globe_container);
-    request_globe_ajax();
+    data = ready_data(institutions);
+    map_globe_spikes(data);
 }
 
-function request_globe_ajax(){
-    $.getJSON('http://inspire-viz.com/python/paperswebglglobe.py',
-                {},
-                map_globe_spikes);
+function ready_data(institutions) {
+    data = [];
+    for (var i = 0; i < institutions.length; i++) {
+        inst = institutions[i];
+        data.push([inst.institution,
+                    [inst.latitude, inst.longitude, inst.magnitude]]);
+    }
 }
 
 function map_globe_spikes(data) {
-      for (i = 0; i < data.length; i++) {
+      for (var i = 0; i < data.length; i++) {
         globe.addData(data[i][1], 'magnitude', data[i][0]);
       }
       globe.createPoints();
